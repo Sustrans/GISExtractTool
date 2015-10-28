cc56292f0d94f5a15bb710742abbfd7d359fb754
Andrew <wilsonandyb@gmail.com>
2015-10-28 16:31:09 +0000

    substitutes basic windows command

--- gis_extract_tool_v1.py
+++ gis_extract_tool_v1.py
@@ -1,6 +1,12 @@
 #print ("hello Nicole, Robert and Andrew")
 import os
 
-input_command = input("give me a command ")
+#orgr2ogr = input("where is ogr2ogr on your computer? ")
 
-os.system(input_command)
+# works for windows
+
+os.system('"C:\path\ogr2ogr" -f "KML" "M:\path\KMLextractfile.kml" "S:\path\INPUTFILE.TAB" -sql "select RouteNumber AS name from INPUTFILE where ((RouteType = 1 or RouteType = 2) and OpenStatus >= 3)' )
+
+
+
+
