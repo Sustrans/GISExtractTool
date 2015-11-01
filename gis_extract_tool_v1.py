c37ae2742e8ba7633d33e8284d327fba2e3d42ba
RW-gis <RW-gis@users.noreply.github.com>
2015-11-01 23:30:45 +0000

    generalises paths with path-to-file

--- gis_extract_tool_v1.py
+++ gis_extract_tool_v1.py
@@ -13,7 +13,7 @@ print ("program start") #for debug purposes
 # os.system('C:') #this fails
 # os.system('cd "C:\Program Files\QGIS Wien\bin\ogr2ogr.exe"') #this fails
 
-os.system('ogr2ogr -f "KML" "C:/Users/Robert Limited/Documents/Temporary Items/KMLextractfile.kml" "C:/Users/Robert Limited/Documents/Temporary Items/TempCycleroutes/CycleRoutes.TAB" -sql "select RouteNumber AS name from CycleRoutes where ((RouteType = 1 or RouteType = 2) and OpenStatus >= 3)' )# in filepaths backslash characters need to be escaped with a second backslash so that the interpreter (I think) is OK with them, or it works to use a single forward slash
+os.system('ogr2ogr -f "KML" "C:/Users/path-to-file/KMLextractfile.kml" "C:/Users/path-to-file/CycleRoutes.TAB" -sql "select RouteNumber AS name from CycleRoutes where ((RouteType = 1 or RouteType = 2) and OpenStatus >= 3)' )# in filepaths backslash characters need to be escaped with a second backslash so that the interpreter (I think) is OK with them, or it works to use a single forward slash
 
 print("program end") #for debug purposes
 
