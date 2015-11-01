a1fd0ded07d21b9eb096e78629deace70682dfe1
RW-gis <RW-gis@users.noreply.github.com>
2015-11-01 18:03:29 +0000

    adding debugging prints to failing program

--- gis_extract_tool_v1.py
+++ gis_extract_tool_v1.py
@@ -1,12 +1,15 @@
 import os
-
+print ("program start") #works
 #orgr2ogr = input("where is ogr2ogr on your computer? ")
 
 # works for windows
-os.system('c:')
-os.system('cd: "C:\Program Files\QGIS Wien\bin"')
+
+os.system('C:') #fails
+print ("Current directory is...") #works
+os.system('cd') #works
+
 #os.system('"C:\path\ogr2ogr" -f "KML" "M:\path\KMLextractfile.kml" "S:\path\INPUTFILE.TAB" -sql "select RouteNumber AS name from INPUTFILE where ((RouteType = 1 or RouteType = 2) and OpenStatus >= 3)' )
 #os.system('dir')
-
+print("program end") #works
 
 
