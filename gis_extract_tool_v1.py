d6957dfb81e8cf9c50cdf5cf04ff8e7bd66d430b
RW-gis <RW-gis@users.noreply.github.com>
2015-11-01 20:19:15 +0000

    adding comments to working version

--- gis_extract_tool_v1.py
+++ gis_extract_tool_v1.py
@@ -3,18 +3,17 @@
 #run the command on the line below
 #    py "z:\path-to-python-program\gis_extract_tool_v1.py"
 #where z is the disk where the python program is
-#This means that ogr2ogr will be available whereas if the current
-#directory is on the z drive I can't access it. 
 
 import os
 print ("program start") #for debug purposes
-#orgr2ogr = input("where is ogr2ogr on your computer? ")
 
+#orgr2ogr = input("where is ogr2ogr on your computer? ")
 # works for windows
 
-# os.system('C:') this fails
+# os.system('C:') #this fails
+# os.system('cd "C:\Program Files\QGIS Wien\bin\ogr2ogr.exe"') #this fails
 
-os.system('ogr2ogr -f "KML" "C:/Users/Robert Limited/Documents/Temporary Items/KMLextractfile.kml" "C:/Users/Robert Limited/Documents/Temporary Items/TempCycleroutes/CycleRoutes.TAB" -sql "select RouteNumber AS name from CycleRoutes where ((RouteType = 1 or RouteType = 2) and OpenStatus >= 3)' )# in file paths backslash characters need to be escaped with a second backslash so that the interpreter (I think) is OK with them, or it works to use a single forward slash
+os.system('ogr2ogr -f "KML" "C:/Users/Robert Limited/Documents/Temporary Items/KMLextractfile.kml" "C:/Users/Robert Limited/Documents/Temporary Items/TempCycleroutes/CycleRoutes.TAB" -sql "select RouteNumber AS name from CycleRoutes where ((RouteType = 1 or RouteType = 2) and OpenStatus >= 3)' )# in filepaths backslash characters need to be escaped with a second backslash so that the interpreter (I think) is OK with them, or it works to use a single forward slash
 
 print("program end") #for debug purposes
 
