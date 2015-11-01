40ee61f3198d7e32a20434f850eb37b40774d186
RW-gis <RW-gis@users.noreply.github.com>
2015-11-01 19:00:06 +0000

    adding slash escapes to file names in ogr2ogr command

--- gis_extract_tool_v1.py
+++ gis_extract_tool_v1.py
@@ -1,15 +1,21 @@
+#temporary instructions
+#on the command line, go to the location of ogr2ogr
+#run the command on the line below
+#    py "z:\path-to-python-program\gis_extract_tool_v1.py"
+#where z is the disk where the python program sits
+#This means that ogr2ogr will be available whereas if the current
+#directory is on the z drive I can't access it. 
+
 import os
-print ("program start") #works
+print ("program start") 
 #orgr2ogr = input("where is ogr2ogr on your computer? ")
 
 # works for windows
 
-os.system('C:') #fails
-print ("Current directory is...") #works
-os.system('cd') #works
+# os.system('C:') this fails
 
-#os.system('"C:\path\ogr2ogr" -f "KML" "M:\path\KMLextractfile.kml" "S:\path\INPUTFILE.TAB" -sql "select RouteNumber AS name from INPUTFILE where ((RouteType = 1 or RouteType = 2) and OpenStatus >= 3)' )
-#os.system('dir')
-print("program end") #works
+os.system('ogr2ogr -f "KML" "C:\\Users\\Robert Limited\\Documents\\Temporary Items\\KMLextractfile.kml" ' )# slash characters need to be escaped with a second slash so that the interpreter (I think) is OK with them
+
+print("program end") 
 
 
